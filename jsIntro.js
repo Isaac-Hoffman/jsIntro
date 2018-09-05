243eb6fda04af770603edcaf026e4aa470efb3bd
Isaac Hoffman <19hoffmani@gmail.com>
2018-09-04 17:41:38 -0700

    got special characters working

---
+++ jsIntro.js
@@ -0,0 +1,26 @@
+let a = 2;
+
+a -= 1;
+
+a++;
+
+let b = 8;
+
+b += 2;
+
+const c = a + b * b;
+
+const d = a * b + b;
+
+const e = a * (b + b);
+
+const f = a * b / a;
+
+const g = b / a * a;
+
+console.log(a, b, c, d, e, f, g);
+
+let Celcius = 100;
+Farenheit = (Celcius * 9/5) + 32;
+alert = Celcius + '\xB0C is ' + Farenheit + '\xB0F'
+console.log(alert);
