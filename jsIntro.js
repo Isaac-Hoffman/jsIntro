f54219788b592269ddc79fb0fdcfd4252e74dd5e
Isaac Hoffman <19hoffmani@gmail.com>
2018-09-04 20:13:08 -0700

    Working FizzBuzz and finished jsIntro

--- jsIntro.js
+++ jsIntro.js
@@ -1,26 +1,44 @@
 let a = 2;
-
 a -= 1;
-
 a++;
-
 let b = 8;
-
 b += 2;
-
 const c = a + b * b;
-
 const d = a * b + b;
-
 const e = a * (b + b);
-
 const f = a * b / a;
-
 const g = b / a * a;
-
-console.log(a, b, c, d, e, f, g);
+console.log('\nValues:')
+console.log('\n' + a, b, c, d, e, f, g);
 
 let Celcius = 100;
 Farenheit = (Celcius * 9/5) + 32;
 alert = Celcius + '\xB0C is ' + Farenheit + '\xB0F'
-console.log(alert);
+console.log('\nCelcius to farenheit:\n' + alert);
+
+let num0 = 0;
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let num4 = 4;
+let num5 = 5;
+let num6 = 6;
+let num7 = 7;
+let num8 = 8;
+let num9 = 9;
+let num10 = 10;
+let num11 = 11;
+let num12 = 12;
+console.log('\nMultiplication table:\n' + num0, num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, num11, num12);
+console.log(num1,num1*num1, num1*num2, num1*num3, num1*num4, num1*num5, num1*num6, num1*num7, num1*num8, num1*num9, num1*num10, num1*num11, num1*num12);
+console.log(num2,num2*num1, num2*num2, num2*num3, num2*num4, num2*num5, num2*num6, num2*num7, num2*num8, num2*num9, num2*num10, num2*num11, num2*num12);
+console.log(num3,num3*num1, num3*num2, num3*num3, num3*num4, num3*num5, num3*num6, num3*num7, num3*num8, num3*num9, num3*num10, num3*num11, num3*num12);
+console.log(num4,num4*num1, num4*num2, num4*num3, num4*num4, num4*num5, num4*num6, num4*num7, num4*num8, num4*num9, num4*num10, num4*num11, num4*num12);
+console.log(num5,num5*num1, num5*num2, num5*num3, num5*num4, num5*num5, num5*num6, num5*num7, num5*num8, num5*num9, num5*num10, num5*num11, num5*num12);
+console.log(num6,num6*num1, num6*num2, num6*num3, num6*num4, num6*num5, num6*num6, num6*num7, num6*num8, num6*num9, num6*num10, num6*num11, num6*num12);
+console.log(num7,num7*num1, num7*num2, num7*num3, num7*num4, num7*num5, num7*num6, num7*num7, num7*num8, num7*num9, num7*num10, num7*num11, num7*num12);
+console.log(num8,num8*num1, num8*num2, num8*num3, num8*num4, num8*num5, num8*num6, num8*num7, num8*num8, num8*num9, num8*num10, num8*num11, num8*num12);
+console.log(num9,num9*num1, num9*num2, num9*num3, num9*num4, num9*num5, num9*num6, num9*num7, num9*num8, num9*num9, num9*num10, num9*num11, num9*num12);
+console.log(num10,num10*num1, num10*num2, num10*num3, num10*num4, num10*num5, num10*num6, num10*num7, num10*num8, num10*num9, num10*num10, num10*num11, num10*num12);
+console.log(num11,num11*num1, num11*num2, num11*num3, num11*num4, num11*num5, num11*num6, num11*num7, num11*num8, num11*num9, num11*num10, num11*num11, num11*num12);
+console.log(num12,num12*num1, num12*num2, num12*num3, num12*num4, num12*num5, num12*num6, num12*num7, num12*num8, num12*num9, num12*num10, num12*num11, num12*num12);
