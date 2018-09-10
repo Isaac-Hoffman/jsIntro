15362463ebdae4f8d2c534ca8e6110e5631cb589
Isaac Hoffman <19hoffmani@gmail.com>
2018-09-09 21:14:28 -0700

    merge

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -4,7 +4,8 @@ while(i < 101){if(i == 0){FizzBuzz = 3;}
         else{if(i % 3 == 0){FizzBuzz = 1;i++;}
             else{if(i % 5 == 0){FizzBuzz = 2;i++;}
                 else{FizzBuzz = 3;}}}}
-    switch(FizzBuzz){case 0:console.log('FizzBuzz');break;
-    case 1:console.log('Fizz');break;
-    case 2:console.log('Buzz');break;
-    default:console.log(i);i++;}}
+    switch(FizzBuzz){
+        case 0:console.log('FizzBuzz');break;
+        case 1:console.log('Fizz');break;
+        case 2:console.log('Buzz');break;
+        default:console.log(i);i++;}}
